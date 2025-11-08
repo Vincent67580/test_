faea3cc814cc11121fe6e728788251b1c1a578c4
Vincent67580 <vbonnet.vincent@gmail.com>
2025-11-08 12:44:25 +0100

    Fix: bind Flask to 0.0.0.0 for Render deployment

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
+import os
 
 app = Flask(__name__)
 
@@ -75,5 +76,9 @@ def contact():
         return redirect(url_for('home'))
     return render_template('contact.html')
 
+
+port = int(os.environ.get("PORT", 5000))
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=port)
